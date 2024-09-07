3d8d4327f9a55251e117d56a4a494a7d2fd6c111
Victor Adriano Placido de Melo <107445881+Victorapm1@users.noreply.github.com>
2024-09-07 19:32:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ for (let dado of dados) {
         resultados += `
     <div class="item-resultado">
 
-        <h2>
+        
             <a href="#" target="_blank">${dado.titulo}</a>
-        </h2>
+        
         <p class="descricao-meta">${dado.descricao}</p>
             <a href=${dado.link} target="_blank">Saiba mais</a>
 
