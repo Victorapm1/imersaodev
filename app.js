5731f393541551d3deeb2c5501ef6c719f02b36b
Victor Adriano Placido de Melo <107445881+Victorapm1@users.noreply.github.com>
2024-09-07 19:13:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+function pesquisar(){
+
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    console.log(campoPesquisa)
+
+    if(campoPesquisa == ""){
+        section.innerHTML = "<p> Digite algum texto</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+
+let resultados = "";
+let titulo = "";
+let descricao = "";
+
+for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+
+        resultados += `
+    <div class="item-resultado">
+
+        <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Saiba mais</a>
+
+    </div>
+    `;
+    }
+
+   if (!resultados) {
+        resultados = "<h2>Nenhum resultado encontrado</h2@>"
+    }
+
+//35:13
+    
+}
+
+section.innerHTML = resultados
+    
+}
+
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal, a Fadinha do Skate Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou um ícone do esporte e uma inspiração para jovens atletas.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+{
+    titulo : "Beatriz Souza",
+    descricao : "Beatriz Souza: A nova estrela do judô, Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a primeira medalha de ouro para o Brasil na competição. Sua determinação e técnica impecável a colocaram no topo do pódio.",
+    link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+},
+{
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Andrade: Um ícone da ginástica brasileira Rebeca Andrade, a joia da ginástica artística brasileira, conquistou o coração dos brasileiros com sua determinação e talento. Nascida em Guarulhos, ela superou diversas lesões para se tornar a maior medalhista olímpica do Brasil.",
+    link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+},
+     ];
